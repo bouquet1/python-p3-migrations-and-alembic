6f41f486653d9a902daac7e76328096379b7c736
bouquet1 <bukuleta@gmail.com>
2023-09-17 09:25:17 -0700

    can't autogenerate

---
+++ lib/migrations/versions/93ec3700f333_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 93ec3700f333
+Revises: 
+Create Date: 2023-09-16 18:35:28.133698
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '93ec3700f333'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
