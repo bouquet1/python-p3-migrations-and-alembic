8aa537adda40a8691f812d4cb012b45ccaa647ff
bouquet1 <bukuleta@gmail.com>
2023-09-19 17:53:06 -0700

    alembic error

--- lib/models.py
+++ lib/models.py
@@ -4,15 +4,14 @@ from sqlalchemy import Column, DateTime, Integer, String, create_engine, desc
 from sqlalchemy.orm import declarative_base
 #from sqlalchemy.ext.declarative import declarative_base (same as the line above)
 
-engine = create_engine('sqlite:///migrations_test.db')
+#engine = create_engine('sqlite:///migrations_test.db')
 
 #define the Base class
 Base = declarative_base()
 
 #Define a Student class and inherit from Base
 class Student(Base):
-    #define the table name using __tablename__
-    __tablename__ = 'students'
+    __tablename__ = "students"
 
     id = Column(Integer(), primary_key=True)
     name = Column(String(), index=True)
@@ -21,10 +20,10 @@ class Student(Base):
     birthday = Column(DateTime())
     enrolled_date = Column(DateTime(), default=datetime.now())
 
-    def __repr__(self):
-        return f"Student {self.id}: " \
-            + f"{self.name}, " \
-            + f"Grade {self.grade}"
+    # def __repr__(self):
+    #     return f"Student {self.id}: " \
+    #         + f"{self.name}, " \
+    #         + f"Grade {self.grade}"
 
 
 
